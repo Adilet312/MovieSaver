e0a7f9931379f889007e3c027df6396c18ed7183
Adilet312 <adiletm@hotmail.com>
2020-09-27 00:32:22 -0700

    Add api key.

--- src/TweeterSaver.js
+++ src/TweeterSaver.js
@@ -1,6 +1,23 @@
 import $ from 'jquery';
 import './css/main.scss';
-const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+$('#search').keyup(function(){
+  let city = $('#search').val();
+
+  // let city = new RegExp(searchArea,'i');
+  console.log(city)
+  // let city = "Seattle";
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+  const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response)
+    }
+  }
+  request.open('GET',url,true);
+  request.send();
+});
 
 
 
