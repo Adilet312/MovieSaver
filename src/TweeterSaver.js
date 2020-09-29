46048ed2de3309e204eb6144dc0745df0a09314e
Adilet312 <adiletm@hotmail.com>
2020-09-29 00:18:59 -0700

    Add movie  API call with fetch and make some styling.

--- src/TweeterSaver.js
+++ src/TweeterSaver.js
@@ -1,23 +1,64 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import './css/main.scss';
-$('#search').keyup(function(){
-  let city = $('#search').val();
 
-  // let city = new RegExp(searchArea,'i');
-  console.log(city)
-  // let city = "Seattle";
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      console.log(response)
-    }
+const search = document.querySelector('#search');
+const outputDiv = document.querySelector('#output');
+const searchMovies = async (title) =>{
+  const url=`http://www.omdbapi.com/?s=${title}&page=1&apikey=${process.env.API_KEY}`;
+  const response = await fetch(url);
+  const movies = await response.json();
+  let result = movies.Search.filter( movie => {
+    let regex = new RegExp(`^${title}`,'gi');
+    return movie.Title.match(regex);
+  });
+  showMovies(result);
+}
+search.addEventListener('input',() => searchMovies(search.value));
+
+function showMovies(result){
+  let ul = document.createElement('ul');
+  if(result.length > 0){
+    let output  = result.map( movie =>`
+      <li>
+        <h2>${movie.Title}</h2>
+        <img src="${movie.Poster}">
+        <p>Holiday movies offer us a glimpse into how the world is could be, often in sharp contrast to our lives as they are. In that way, the annual act of viewing them is like a religious ritual.</p>
+      </li>
+    `).join('');
+    ul.innerHTML = output;
+    outputDiv.appendChild(ul);
   }
-  request.open('GET',url,true);
-  request.send();
-});
+  else{
+    ul.innerHTML = '';
+  }
+}
+// function showMovies(result){
+//   result.forEach(movie =>{
+//     let li  = document.createElement('li');
+//     let h2  = document.createElement('h2');
+//     h2.innerText = movie.Title;
+//     let img = document.createElement('img');
+//     img.src = movie.Poster;
+//     li.appendChild(h2);
+//     li.appendChild(img);
+//     output.appendChild(li);
+//   })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
